bb4ae2875ba771fcf190238025cfb560743b647f
congshuanglong <congshuanglong@126.com>
2015-11-19 01:37:11 +0800

    update

---
+++ src/main/java/com/felit/ListenerForFlow.java
@@ -0,0 +1,14 @@
+package com.felit;
+
+import org.activiti.engine.delegate.DelegateExecution;
+import org.activiti.engine.delegate.ExecutionListener;
+
+public class ListenerForFlow implements ExecutionListener {
+
+	@Override
+	public void notify(DelegateExecution arg0) throws Exception {
+		
+		
+	}
+
+}
