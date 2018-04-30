3556d000d5a738dff68886d7d266516e146d6bc0
congshuanglong <congshuanglong@126.com>
2018-04-30 10:35:54 +0800

    添加TODO注释

--- src/test/java/com/livedrof/activiti/Sayhelloleave2.java
+++ src/test/java/com/livedrof/activiti/Sayhelloleave2.java
@@ -23,6 +23,8 @@ import static org.junit.Assert.assertNotNull;
  * 5、FormService: 表单Service
  * 6、HistoryService:历史Service
  * 7、ManagementService:  引擎管理Service
+ * <p>
+ * 以作务的形式驱动业务系统的自动完成，活动是业务流程里的最小组成部分。
  */
 public class Sayhelloleave2 {
     @Test
@@ -37,31 +39,31 @@ public class Sayhelloleave2 {
         assertEquals("leavesayhello", processDefinition.getKey());
         // 启动流程并返回实例
         RuntimeService runtimeService = processEngine.getRuntimeService();
-
+        //此为流程变量，
         Map<String, Object> vars = new HashMap<String, Object>();
         vars.put("applyUser", "employee1");
         vars.put("days", 3);
 
-        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("leavesayhello",vars);
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("leavesayhello", vars);
         assertNotNull(processInstance);
 
         System.out.println("pid=" + processInstance.getId() + ", pdid=" + processInstance.getProcessDefinitionId());
-
         TaskService taskService = processEngine.getTaskService();
         Task task = taskService.createTaskQuery().taskCandidateGroup("deptLeader").singleResult();
         // 签收此任务归用户leaderUser所有
         taskService.claim(task.getId(), "leaderUser");
-
+        // 任务变量
         Map<String, Object> variables = new HashMap<String, Object>();
         variables.put("approved", true);
 
         taskService.complete(task.getId(), variables);
         // TODO 怎样取得变量，流程的变量，流程里面的变量
         System.out.println("打印变量：");
-        System.out.println( ((ExecutionEntity)processInstance).getVariables());
+        System.out.println(((ExecutionEntity) processInstance).getVariables());
 
         HistoryService historyService = processEngine.getHistoryService();
         HistoricProcessInstanceQuery historicProcessInstanceQuery = historyService.createHistoricProcessInstanceQuery().finished();
+        //TODO 怎样查找完整的历史信息
         System.out.println(historicProcessInstanceQuery.count());
     }
 }
